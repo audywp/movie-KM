6899c7bb0621ce0ee7b13b735fd24755ca01de31
audy <audy.wisuda@warungpintar.co>
2022-03-15 22:56:54 +0700

    initialize

---
+++ src/helpers/apiAccessToken.js
@@ -0,0 +1,6 @@
+export const ACCESS_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyN2Q4YjA1NjhjMzIyYTkxOTUzN2JkMWM0MDhiMWRhOCIsInN1YiI6IjVlZDEyYWM5M2RjMzEzMDAxZmNjZDI1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t_jB76z5LkR-ayjxfGxfChGwCvY3e3PwEYTu4EuDJZk';
+
+export const BaseUrl = 'https://api.themoviedb.org/3';
+
+export const ImageUrl = 'https://image.tmdb.org/t/p/original';
---
+++ src/helpers/isLessThan3.js
@@ -0,0 +1,7 @@
+export const isLessThan3 = endData => {
+  if (endData / 2 < 3) {
+    return false;
+  } else {
+    return true;
+  }
+};
